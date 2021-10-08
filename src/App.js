76a47a04388161fcfc1751746480cf4b277bfe7f
Vlad <vladbatalinontech@gmail.com>
2021-10-08 08:15:14 +0300

    trying to add the new project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './styles/HomePage/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="main-nav-bar">
+        <ul>
+          <li>home (logo)</li>
+          <li>about</li>
+          <li>services</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+      <header className="App-header">
+        <h1> LETS GO !
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
