a88283f4f423295efc98f8f36259271141fa88d5
BatalinVlad <vladbatalinontech@gmail.com>
2021-10-27 07:07:04 +0300

    send email working now with emailJs

--- src/cmps/SendMailNow.js
+++ src/cmps/SendMailNow.js
@@ -1,12 +1,40 @@
 import React from 'react';
+import emailjs from 'emailjs-com';
 
-function SendMailNow({onSetToggleSendMail}) {
+import { VscChromeMinimize } from 'react-icons/vsc'
+
+function SendMailNow({ onSetToggleSendMail }) {
+
+    function sendEmail(e) {
+        e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
+
+        emailjs.sendForm('service_7nje94c', 'template_0xruass', e.target, 'user_SkozIqo4hqkjis8FWw2Lx')
+            .then( res => {
+                console.log(res);
+            }).catch(err => console.log(err));
+    }
+
     return (
         <>
             <div className="send-mail-now container flex column align-center">
-              <h1 className="text-center">  hey send mail now! </h1>
+                <h1 className="text-center uppercase">send me an e-mail</h1>
+                <form className="contact-form" onSubmit={sendEmail}>
+                    {/* <input type="hidden" name="contact_number" /> */}
+                    <label>name</label>
+                    <input type="text" name="name" />
+
+                    <label>Email</label>
+                    <input type="email" name="user_email" />
+                    {/*                     
+                    <label>Subject</label>
+                    <input type="text" name="subject" /> */}
+
+                    <label>Message</label>
+                    <textarea name="message" rows="4" />
+                    <input type="submit" className="get-start-btn btn2" value="Send Now" />
+                </form>
                 <div className="send-mail-now-exit" onClick={onSetToggleSendMail}>
-                <h1>X</h1>
+                    <VscChromeMinimize />
                 </div>
             </div>
         </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,11 +21,11 @@ function Home() {
                 </div>
                 <div className="home-page-header-getstart flex column fill-height">
                     <h1 className="text-center bold">fitness coach and healthy life advisor for any goal!</h1>
-                    <p><span className="capitalize">train</span> with me to get the best reasults in least amount of time as possible!
+                    <p className="capitalize bold">train with me to get the best reasults in least amount of time as possible!
                         together with my speciel meal plans and training routin we will get your goal in easy and fun way!
                     </p>
                     <div className="get-srtat-btn-container flex justify-items align-center">
-                        <button className="get-start-btn uppercase">get start now!</button>
+                        <button className="get-start-btn btn1 uppercase">get start now!</button>
                     </div>
                 </div>
             </div>
